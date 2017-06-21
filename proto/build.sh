d9c57225d9675eaaef299fd368a34496155aa7af
Anton Grbin <anton.grbin@gmail.com>
2017-06-21 18:26:32 +0000

    generating js and cc proto files

--- proto/build.sh
+++ proto/build.sh
@@ -1,1 +1,9 @@
-protoc --js_out=import_style=commonjs,binary:generated voronoi.proto
+protoc \
+  --js_out=import_style=commonjs,binary:generated_js \
+  --cpp_out=generated_cc \
+  voronoi.proto
+
+protoc \
+  --js_out=import_style=commonjs,binary:generated_js \
+  --cpp_out=generated_cc \
+  descriptor.proto 
